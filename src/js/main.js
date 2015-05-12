2ce282aefd503fb3b657f13529904431cb571314
Andre <cnandre.nunes@gmail.com>
2015-05-12 02:14:40 -0300

    responsive menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,26 @@
 var app = angular.module('AutoGeoAPP', []);
 
 app.controller('MainCtrl', function ($scope){
+	
+	$scope.enableMenu = false;
 
-	$scope.add = function () {
-		alert("Novo");
+});
+
+app.directive('toggleMenuIcon', function (){
+	return{
+		restric		: 'A',
+		scope		: false,
+		link		: function (scope, elem, attrs){
+			elem.on('click', function(){
+				if(scope.enableMenu === true){
+					elem[0].firstElementChild.className = "glyphicon glyphicon-menu-hamburger blue";
+				}
+				if(scope.enableMenu === false){
+					elem[0].firstElementChild.className = "glyphicon glyphicon-remove blue";
+				}
+				scope.enableMenu = !scope.enableMenu
+			});
+
+		} 	
 	}
 });
