1bba8522f51e4737d5c529a7b5c0cbca8461c93c
Andre <cnandre.nunes@gmail.com>
2015-05-19 02:06:02 -0300

    interface

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,59 @@
 var app = angular.module('AutoGeoAPP', ["leaflet-directive"]);
 
-app.controller('MainCtrl', [ '$scope', '$http', function($scope, $http) {
+app.controller('MainCtrl', [ '$scope', '$http', '$filter', function($scope, $http, $filter) {
 	
+    var icon = {  
+        iconUrl:'build/img/marker-icon.png',
+        iconSize:[25, 41],
+        iconAnchor:[12, 0]  
+    }; 
+
 	$scope.enableMenu = false;
-
-
+    $scope.search = '';
+    $scope.carros = {};
 
 	//leaflet
 	angular.extend($scope, {
         poa: {
             lat: -30.0257548,
-            lng: -51.0733013,
-            zoom: 11
+            lng: -51.1833013,
+            zoom: 12
         },
         defaults: {
             scrollWheelZoom: true
         }
     });
 
-
     // Get the countries geojson data from a JSON
-    $http.get("data/json/macrozona.geojson.json").success(function(data, status) {
+    $http.get("data/json/carros.json").success(function(data, status) {
         angular.extend($scope, {
-            poaBairros: {
-                data: data,
-                style: {
-                    fillColor: "green",
-                    weight: 2,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.7
-                }
-            }
-        });
+          data: data,
+          carros: {
+            data:data,
+            style:
+                function (feature) {
+                    return {};
+                },
+                pointToLayer: function(feature, latlng) {
+                    return new L.marker(latlng, {icon: L.icon(icon)});
+                },
+                onEachFeature: function (feature, layer) {
+                    layer.bindPopup("<h1>" +feature.properties.modelo+" / "+feature.properties.ano+"</h1>");
+                } 
+          } //geojson
+        });         
+    });
+
+    $scope.$watch('search', function (newVal, oldVal) {
+      if (newVal !== oldVal && newVal !== '') {
+        $scope.carros.data = $filter('filter')($scope.data, 'modelo', newVal);
+      } else {
+        $scope.carros.data = $scope.data;
+      }
+    });
+
+    $scope.$on("leafletDirectiveMap.geojsonClick", function (ev, featureSelected, leafletEvent) {
+        console.log(featureSelected);
     });
 
 }]);
@@ -58,3 +78,21 @@ app.directive('toggleMenuIcon', function (){
 		}
 	}
 });
+
+app.filter('filter', [function() {
+  return function(geojson, searchProperty, searchValue) {
+    console.log('Matches:');
+    var matches = {'type': 'FeatureCollection', 'features': []};
+    angular.forEach(geojson.features, function(featureObject, featureKey) {
+      if (featureObject.properties.hasOwnProperty(searchProperty)) {
+        var property = featureObject.properties[searchProperty].toLowerCase();
+        var search = searchValue.toLowerCase();
+        if (property.indexOf(search) > -1) {
+          matches.features.push(featureObject);
+          console.log(featureObject.properties.NAME);
+        }
+      }
+    });
+    return matches;
+  };
+}]);
