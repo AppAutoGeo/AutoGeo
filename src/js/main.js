5136cac40c3df44b3029f5bdaabb44d805566b70
Andre <cnandre.nunes@gmail.com>
2015-05-27 01:00:37 -0300

    details

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,45 @@
 var app = angular.module('AutoGeoAPP', ["leaflet-directive"]);
 
-app.controller('MainCtrl', [ '$scope', '$http', '$filter', function($scope, $http, $filter) {
-	
+app.controller('MainCtrl', [ '$scope', '$http', '$filter', '$rootScope', 'ServicoAnuncios', function($scope, $http, $filter, $rootScope, ServicoAnuncios) {
+    
     var icon = {  
         iconUrl:'build/img/marker-icon.png',
         iconSize:[25, 41],
         iconAnchor:[12, 0]  
     }; 
 
-	$scope.enableMenu = false;
+    $scope.enableMenu = false;
     $scope.search = '';
-    $scope.carros = {};
+    $scope.markers = [];
+    
+    var promiseAnuncios = ServicoAnuncios.getAnuncios();
+    promiseAnuncios.then(function(data) {
+        $rootScope.anuncios = data.anuncios;
+        angular.forEach(data.anuncios, function(anuncio, i) {
+            
+            $scope.markers.push({
+                lat: anuncio.geometry.coordinates[1], 
+                lng: anuncio.geometry.coordinates[0], 
+                message: "<popup anuncio='anuncios[" + i + "]'></popup>",
+                popupOptions: {minWidth: 300, maxWidth: 300}
+            });
+        });
+    });
 
-	//leaflet
-	angular.extend($scope, {
+    var url = 'data/json/carros.json';
+    $http.get(url).success(function(anuncios){
+        angular.forEach(anuncios.features, function(anuncio, i) {
+            $scope.markers.push({
+                lat: anuncio.geometry.coordinates[0], 
+                lng: anuncio.geometry.coordinates[1], 
+                message: "<popup anuncio='anuncios[" + i + "]'></popup>"
+            });
+        });
+    }).error(function(msg, code) {
+        console.log(msg+code);
+    });
+
+    angular.extend($scope, {
         poa: {
             lat: -30.0257548,
             lng: -51.1833013,
@@ -21,62 +47,39 @@ app.controller('MainCtrl', [ '$scope', '$http', '$filter', function($scope, $htt
         },
         defaults: {
             scrollWheelZoom: true
-        }
+        },
     });
 
-    // Get the countries geojson data from a JSON
-    $http.get("data/json/carros.json").success(function(data, status) {
-        angular.extend($scope, {
-          data: data,
-          carros: {
-            data:data,
-            style:
-                function (feature) {
-                    return {};
-                },
-                pointToLayer: function(feature, latlng) {
-                    return new L.marker(latlng, {icon: L.icon(icon)});
-                },
-                onEachFeature: function (feature, layer) {
-                    layer.bindPopup("<h1>" +feature.properties.modelo+" / "+feature.properties.ano+"</h1>");
-                } 
-          } //geojson
-        });         
-    });
-
-    $scope.$watch('search', function (newVal, oldVal) {
+    /*$scope.$watch('search', function (newVal, oldVal) {
       if (newVal !== oldVal && newVal !== '') {
         $scope.carros.data = $filter('filter')($scope.data, 'modelo', newVal);
       } else {
         $scope.carros.data = $scope.data;
       }
-    });
-
-    $scope.$on("leafletDirectiveMap.geojsonClick", function (ev, featureSelected, leafletEvent) {
-        console.log(featureSelected);
-    });
+    });*/
+    
 
 }]);
 
 app.directive('toggleMenuIcon', function (){
-	return{
-		restric		: 'E',
-		scope		: false,
-		template	: '<div id="menuMobile" class="visible-xs-block" ng-click="toggleMenu()">'
-						+'<span class="glyphicon glyphicon-menu-hamburger blue"></span>'
-						+'<span class="txtBtnMenu">MENU</span>'
-						+'</div>',
-		link		: function(scope, elem, attrs){
-			elem.on('click', function(){
-				if(scope.enableMenu){
-					elem[0].firstElementChild.firstElementChild.className = "glyphicon glyphicon-menu-hamburger blue";
-				}else{
-					elem[0].firstElementChild.firstElementChild.className = "glyphicon glyphicon-remove blue";
-				}
-				scope.$apply(scope.enableMenu = !scope.enableMenu); 
-			});
-		}
-	}
+    return{
+        restric     : 'E',
+        scope       : false,
+        template    : '<div id="menuMobile" class="visible-xs-block" ng-click="toggleMenu()">'
+                        +'<span class="glyphicon glyphicon-menu-hamburger blue"></span>'
+                        +'<span class="txtBtnMenu">MENU</span>'
+                        +'</div>',
+        link        : function(scope, elem, attrs){
+            elem.on('click', function(){
+                if(scope.enableMenu){
+                    elem[0].firstElementChild.firstElementChild.className = "glyphicon glyphicon-menu-hamburger blue";
+                }else{
+                    elem[0].firstElementChild.firstElementChild.className = "glyphicon glyphicon-remove blue";
+                }
+                scope.$apply(scope.enableMenu = !scope.enableMenu); 
+            });
+        }
+    }
 });
 
 app.filter('filter', [function() {
@@ -96,3 +99,34 @@ app.filter('filter', [function() {
     return matches;
   };
 }]);
+
+app.directive('popup', ['$http', '$compile', function($http, $compile) {
+    return {
+        restrict: 'E',
+        scope: {
+            anuncio: "="
+        },
+        templateUrl: 'popup.html'
+    };
+}]);
+
+app.factory('ServicoAnuncios', function($http, $q) {
+    return {
+        getAnuncios: function() {
+            var d = $q.defer();
+            var url = 'data/json/carros.json';
+            var saida = { anuncios: [] };
+            $http.get(url)
+            .success(function(anuncios){
+                angular.forEach(anuncios.features, function(anuncio) {
+                    saida.anuncios.push(anuncio);
+                });
+                d.resolve(saida);
+            })
+            .error(function(msg, code) {
+                d.reject(msg);
+            });
+            return d.promise;
+        }
+    };
+});
